05c2b5dfcb551006bb8d750deb7d44ddb0379dc2
Evan Sharp <evan@packetmonkey.org>
2014-06-12 14:52:22 -0400

    Test error

--- ext/image/extconf.rb
+++ ext/image/extconf.rb
@@ -1,7 +1,11 @@
 require "mkmf"
 
-find_header "magick_wand.h",
+found = find_header "magick_wand.h",
   "/app/vendor/graphicsmagick/include/GraphicsMagick/wand"
+
+unless found
+  throw "Could not find wand/magick_wand.h"
+end
 
 pkg_config("GraphicsMagickWand")
 create_makefile("image")
