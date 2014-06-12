254d5d707a03d4eabdc858b20fd5e09331aeeccf
Evan Sharp <evan@packetmonkey.org>
2014-06-12 17:18:33 -0400

    wip

--- ext/image/extconf.rb
+++ ext/image/extconf.rb
@@ -1,16 +1,14 @@
 require "mkmf"
 
-puts "Current working directory - #{Dir.pwd}"
-puts 'All possible directories'
-puts Dir.glob("/tmp/*")
-
-found = find_header "wand/magick_wand.h",
-  "/app/vendor/graphicsmagick/include/GraphicsMagick",
-  "/usr/local/Cellar/graphicsmagick/1.3.19_1/include/GraphicsMagick"
-
-unless found
-  throw "Could not find wand/magick_wand.h"
-end
+# puts "Current working directory - #{Dir.pwd}"
+# puts 'All possible directories'
+# puts Dir.glob("/tmp/*")
+# found = find_header "wand/magick_wand.h",
+#   "/app/vendor/graphicsmagick/include/GraphicsMagick",
+#   "/usr/local/Cellar/graphicsmagick/1.3.19_1/include/GraphicsMagick"
+# unless found
+#   throw "Could not find wand/magick_wand.h"
+# end
 
 pkg_config("GraphicsMagickWand")
 create_makefile("image")
