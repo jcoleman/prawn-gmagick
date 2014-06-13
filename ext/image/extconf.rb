1b42148c751a5630908b9ed64a64f9f86275477d
Evan Sharp <evan@packetmonkey.org>
2014-06-13 09:28:19 -0400

    wip

--- ext/image/extconf.rb
+++ ext/image/extconf.rb
@@ -2,19 +2,22 @@ require 'pathname'
 require "mkmf"
 
 if ENV['BUNDLE_CONFIG']
-  puts "=-=-=-=-=-=-="
-  puts "Current working directory - #{Dir.pwd}"
-  puts "=-=-=-=-=-=-="
-  puts ENV.to_h
-  puts "=-=-=-=-=-=-="
+  # puts "=-=-=-=-=-=-="
+  # puts "Current working directory - #{Dir.pwd}"
+  # puts "=-=-=-=-=-=-="
+  # puts ENV.to_h
+  # puts "=-=-=-=-=-=-="
   bundle_config_path = ENV['BUNDLE_CONFIG']
-  puts "bundle_config_path #{bundle_config_path}"
-  puts "=-=-=-=-=-=-="
+  # puts "bundle_config_path #{bundle_config_path}"
+  # puts "=-=-=-=-=-=-="
   build_dir = Pathname.new(bundle_config_path).parent.parent
-  puts "Build dir: #{build_dir}"
-  puts "=-=-=-=-=-=-="
-  puts "Build dir entries: #{build_dir.entries}"
-  puts "=-=-=-=-=-=-="
+  # puts "Build dir: #{build_dir}"
+  # puts "=-=-=-=-=-=-="
+  # puts "Build dir entries: #{build_dir.entries}"
+  # puts "=-=-=-=-=-=-="
+
+  find_header "wand/magick_wand.h",
+    build_dir.join("include/GraphicsMagick")
 end
 
 # puts Dir.glob("/tmp/*")
